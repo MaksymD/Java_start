d1550edb0534451fb03b925bd22225efa03fa775
maksymd <maksym.dotsenko@gmail.com>
2020-06-15 14:34:43 +0200

    lesson 3 add

---
+++ src/lesson3/IfAndElse.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class IfAndElse {
+    //https://www.javatpoint.com/java-if-else
+    public static void main(String[] args) {
+        Scanner age = new Scanner(System.in);
+        System.out.println("Tell me your age: ");
+        int i = age.nextInt();
+        System.out.println("Your age is: "+i);
+    }
+
+}
